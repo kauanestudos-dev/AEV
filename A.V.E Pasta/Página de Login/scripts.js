a1b6523290bf6048c837e2ed2d595243bef440f7
Matheus Santos <matheussantosn09@gmail.com>
2026-02-26 19:51:45 -0300

    Add files via upload

---
+++ A.V.E Pasta/Página de Login/scripts.js
@@ -0,0 +1,15 @@
+
+let email = document.querySelector(".form-group email")
+let cpf = document.querySelector(".form-group cpf")
+let senha = document.querySelector(".form-group senha")
+
+let voltar = document.querySelector(".login-link")
+
+function Guardar(){
+
+    let email
+
+console.log(email)
+}
+
+Guardar()
