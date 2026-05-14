2c4bcd59943b850f8702a76aa5f59c0b4f88ca9f
Matheussantosn0 <matheussantosn09@gmail.com>
2026-05-14 19:16:14 -0300

    mudanças

---
+++ java/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome;
+        String email;
+       Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem Vindo ao AEV sua agenda escolar");
+
+        System.out.println();
+
+
+
+        
+
+
+    }
+}
